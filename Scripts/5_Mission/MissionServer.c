ade933eac9470739fc11966884b7cc0a0afa9cee
hunter688 <79998022+hunter688@users.noreply.github.com>
2022-06-11 14:18:05 +0200

    added random select of config

--- Scripts/5_Mission/MissionServer.c
+++ Scripts/5_Mission/MissionServer.c
@@ -3,7 +3,26 @@ modded class MissionServer
 	void MissionServer()
     {
 		BuBuConfig config;
-		BubakConfig.UpgradeConfig("SpawnerBubaku.json", config);
+		FileAttr fileAttr;
+		string fileName;
+		TStringArray fajly;
+		fajly = new TStringArray();
+		FindFileHandle handle = FindFile("$profile:\\SpawnerBubaku\\SpawnerBubaku*.json", fileName, fileAttr, 0);
+		fajly.Insert(fileName);
+		//SPBLogger.GetInstance().Log("Filename " + fileName, SPBLogger.LOGLEVEL_CRITICAL);
+		while ( FindNextFile(handle, fileName, fileAttr))
+		{
+			fajly.Insert(fileName);
+			//SPBLogger.GetInstance().Log("Filename " + fileName, SPBLogger.LOGLEVEL_CRITICAL);
+		}
+		fileName = fajly.GetRandomElement();
+		fileName = fajly.GetRandomElement();
+		if (fajly.Count()>1)
+		{
+			SPBLogger.GetInstance().Log("Selected Filename " + fileName, SPBLogger.LOGLEVEL_CRITICAL);
+		}
+		
+		BubakConfig.UpgradeConfig(fileName, config);
         GetDayZGame().SetBubakConfig(config);
 
 		if (GetGame().IsServer()) 
